a43ef1a4b6eb51e6e87c1be96f77ece69a6037bb
Panayotis Vryonis <58812+vrypan@users.noreply.github.com>
2024-10-14 21:39:47 +0300

    v0.7.12
    - fario-userdata: get/set USER_DATA_TYPES
    - fario-userdata uses click instead of argparse. Will convert the
    project to click, pice by piece.

--- fario/fario_userdata.py
+++ fario/fario_userdata.py
@@ -1,22 +1,16 @@
-#!/usr/bin/env python
-import sys
+# SPDX-FileCopyrightText: 2024-present Panayotis Vryonis <vrypan@gmail.comm>
+#
+# SPDX-License-Identifier: MIT
+import os
+import click
 import time
+from dotenv import load_dotenv
+
+from . __about__ import version
+from . config import get_conf
+from farcaster import Message, FARCASTER_EPOCH
 from farcaster.HubService import HubService
 from farcaster.fcproto.message_pb2 import SignatureScheme, HashScheme, UserDataType
-from farcaster import Message, FARCASTER_EPOCH
-import argparse
-from . config import get_conf
-from . __about__ import version
-
-
-"""
-USER_DATA_TYPE_NONE = 0;
-USER_DATA_TYPE_PFP = 1; // Profile Picture for the user
-USER_DATA_TYPE_DISPLAY = 2; // Display Name for the user
-USER_DATA_TYPE_BIO = 3; // Bio for the user
-USER_DATA_TYPE_URL = 5; // URL of the user
-USER_DATA_TYPE_USERNAME = 6; /
-"""
 
 USER_DATA_TYPES = {
     'none': UserDataType.USER_DATA_TYPE_NONE,
@@ -27,42 +21,59 @@ USER_DATA_TYPES = {
     'username': UserDataType.USER_DATA_TYPE_USERNAME
 }
 
-def main():
-    parser = argparse.ArgumentParser(prog="fario-userdata", description="Get/Set userdata")
-    parser.add_argument('--version', action='version', version='%(prog)s v'+version)
-    parser.add_argument("--hub", help="Use the hub at <ADDRESS>. Ex. --hub 192.168.1.1:2283", type=str)
-    parser.add_argument("--ssl", help="Use SSL", action="store_true")
-    parser.add_argument("--signer", type=str, help="Signer's private key")
-    parser.add_argument("--user-fid", type=int, help="User's fid")
-    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
-    parser.add_argument("key", type=str, help="Data type")
-    parser.add_argument("value", type=str, help="Value")
-    args = parser.parse_args()
+@click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
+@click.version_option(version=version, prog_name="fario-user-data")
+def fario_user_data():
+    """Farcaster Network Mapper
+    """
+    CONFIG_FILE = os.path.expanduser('~/.fario')
+    load_dotenv(CONFIG_FILE)
+    load_dotenv()
     
-    conf=get_conf(required=['hub', 'user_fid', 'signer'], args=args)
-    user_fid = conf['user_fid']
+
+@fario_user_data.command()
+@click.option('--hub', default='hoyt.farcaster.xyz:2283', help="Use the hub at <ADDRESS:PORT>. Ex. --hub 192.168.1.1:2283", envvar='hub')
+@click.option('--ssl', is_flag=True, default=False, help="Use SSL")
+@click.option('--signer', help="Signer's private key", required=True, envvar='signer')
+@click.argument('user-fid', type=click.INT)
+@click.argument('data-type', type=click.Choice([k for k in USER_DATA_TYPES.keys()], case_sensitive=False), required=True)
+@click.argument('data-value')
+def set(hub, ssl, signer, user_fid, data_type, data_value):
 
     timestamp = int(time.time())-FARCASTER_EPOCH
-    
-    hub = HubService(conf['hub'], use_async=False, use_ssl=conf['ssl'])
+    hub = HubService(hub, use_async=False, use_ssl=ssl)
 
     message_builder = Message.MessageBuilder(
         HashScheme.HASH_SCHEME_BLAKE3, 
         SignatureScheme.SIGNATURE_SCHEME_ED25519, 
-        bytes.fromhex(conf['signer'][2:])
+        bytes.fromhex(signer[2:])
     )
     
     data = message_builder.user_data.add(
-        fid = int(user_fid), 
-        data_type = USER_DATA_TYPES[args.key],
-        data_value = args.value
+        fid = user_fid, 
+        data_type = USER_DATA_TYPES[data_type],
+        data_value = data_value
     )
 
     msg  = message_builder.message(data)
 
     ret  = hub.SubmitMessage(msg)
-    if args.verbose:
-        print(f"{USER_DATA_TYPES[key]} set to {value}")
+    
+    print(f"USER_DATA_TYPE_{data_type.upper()} set to {data_value}")
 
-if __name__ == '__main__':
-    main()
+
+@fario_user_data.command()
+@click.option('--hub', default='hoyt.farcaster.xyz:2283', help="Use the hub at <ADDRESS:PORT>. Ex. --hub 192.168.1.1:2283", envvar='hub')
+@click.option('--ssl', is_flag=True, default=False, help="Use SSL")
+@click.argument('user-fid', type=click.INT)
+@click.argument('data-type', type=click.Choice([k for k in USER_DATA_TYPES.keys()], case_sensitive=False), required=True)
+def get(hub, ssl, user_fid, data_type):
+    hub = HubService(hub, use_async=False, use_ssl=ssl)
+    response = hub.GetUserDataByFid(user_fid, 100)
+    for c in response.messages:
+        if c.data.user_data_body.type == USER_DATA_TYPES[data_type]:
+            print(c.data.user_data_body.value)
+
+
+
+
